d7161a70ed6c2f8dcbe89f9b6bad2ef6cc5b5d94
lab_win <lab_win>
2021-09-05 15:48:01 +0800

    add CUT module for 3dresnet testing

---
+++ gan_model.py
@@ -0,0 +1,14 @@
+# ==========================================
+
+
+
+cut_model = []
+cut_transform = []
+
+
+def running():
+    global cut_model, cut_transform
+    
+
+if __name__ == '__main__':
+    running()
